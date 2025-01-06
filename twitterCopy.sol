4618de27f9977b66cdc2b2ead9db74ebf637c3e0
EduVemba <eduvemba18@gmail.com>
2025-01-06 20:55:15 +0000

    feat: Minor changes

--- twitterCopy.sol
+++ twitterCopy.sol
@@ -5,18 +5,28 @@ pragma solidity ^0.8.0;
 
 contract Twitter{
 
-    mapping(address => string) private UserTwetts;
+    mapping(address => string[]) private UserTwetts;
+
+    address owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
 
     event newTweet(string indexed notify, address indexed tweetHost);
 
-    function createTweet(string memory tweet) external payable {
-        require(msg.value >= 1 wei);
-        UserTwetts[msg.sender] = tweet;
+    function createTweet(string memory tweet) external {
+        UserTwetts[msg.sender].push(tweet);
         emit newTweet("New Tweet by: ", msg.sender);
+    
     }
 
-    function getTweet(address user)external view returns (string memory){
-        return UserTwetts[user];
+    function getTweet(address user, uint _i)external view returns (string memory){
+      return UserTwetts[user][_i];
+    }
+
+    function getAllTwetts(address _owner) external view returns (string[] memory){
+        return UserTwetts[_owner];
     }
 
 }
