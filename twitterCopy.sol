d1b71f4147a8910feec0ce96cbb84f65f314252a
EduVemba <eduvemba18@gmail.com>
2025-01-06 21:48:39 +0000

    feat: Tweet struct and functions changes

--- twitterCopy.sol
+++ twitterCopy.sol
@@ -5,28 +5,38 @@ pragma solidity ^0.8.0;
 
 contract Twitter{
 
-    mapping(address => string[]) private UserTwetts;
-
-    address owner;
-
-    constructor(){
-        owner = msg.sender;
+    struct Tweet {
+        address  author;
+        string   content;
+        uint256  TimeStamp;
+        uint256  likes ;
     }
+
+    mapping(address => Tweet[]) private UserTwetts;
+
 
     event newTweet(string indexed notify, address indexed tweetHost);
 
-    function createTweet(string memory tweet) external {
-        UserTwetts[msg.sender].push(tweet);
+    function createTweet(string memory _tweet) external {
+        
+        Tweet memory newTwet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            TimeStamp: block.timestamp,
+            likes: 0
+        });
+        
+        UserTwetts[msg.sender].push(newTwet);
         emit newTweet("New Tweet by: ", msg.sender);
     
     }
 
-    function getTweet(address user, uint _i)external view returns (string memory){
+    function getTweet(address user, uint _i)external view returns (Tweet memory){
       return UserTwetts[user][_i];
     }
 
-    function getAllTwetts(address _owner) external view returns (string[] memory){
+    function getAllTwetts(address _owner) external view returns (Tweet[] memory){
         return UserTwetts[_owner];
     }
 
-}
+} 
