488f3267d7c54485679baaa97c9373c6bff641af
EduVemba <eduvemba18@gmail.com>
2025-01-08 22:39:37 +0000

    fix: setProfile, mistaken

--- Profile.sol
+++ Profile.sol
@@ -14,13 +14,6 @@ contract Profile {
 
     function setProfile(string memory _displayName, string memory _bio) public {
         profiles[msg.sender] = UserProfile(_displayName,_bio);
-
-        UserProfile memory newUserProfile = UserProfile ({
-            displayName: _displayName,
-            bio: _bio
-        });
-
-        profiles[msg.sender] = newUserProfile;
     }
  
     function getProfile(address _user) public view returns (UserProfile memory) {
